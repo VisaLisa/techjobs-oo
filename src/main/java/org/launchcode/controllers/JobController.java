2d9ce59413e6ff436e42632715bedf388c023f84
Visakha Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2017-08-21 18:33:45 -0700

    inital commit

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
